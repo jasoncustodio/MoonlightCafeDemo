56c01d71df9ee74ebd010dad34c97f4b71567d7d
Jason Custodio <jasoncustodio@Jasons-iMac.local>
2017-12-14 18:26:49 -0800

    Added Cells

---
+++ MoonlightCafeDemo/ArtistTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ArtistTableViewCell.swift
+//  MoonlightCafeDemo
+//
+//  Created by Jason Custodio on 12/14/17.
+//  Copyright © 2017 Jason Custodio. All rights reserved.
+//
+
+import UIKIT
