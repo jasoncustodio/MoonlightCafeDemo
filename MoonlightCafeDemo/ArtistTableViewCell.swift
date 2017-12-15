9abd23fa476ef63434b374e6855008841816e2da
Jason Custodio <jasoncustodio@Jasons-iMac.local>
2017-12-14 19:37:45 -0800

    View Cell

---
+++ MoonlightCafeDemo/ArtistTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistTableViewCell.swift
+//  MoonlightCafeDemo
+//
+//  Created by Jason Custodio on 12/14/17.
+//  Copyright © 2017 Jason Custodio. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var profileImageView: UIImageView!
+
+  @IBOutlet weak var descriptionLabel: UILabel!
+
+}
